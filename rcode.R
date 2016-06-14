05227cdeb690dd298af893a02e847d0f5c3cc42d
Ashley Noel Hinton <ahin017@aucklanduni.ac.nz>
2016-06-14 13:54:47 +1200

    simple input host example

--- rcode.R
+++ rcode.R
@@ -1,9 +1,19 @@
 library(conduit)
 
-## simpleInputHost
+## simpleInputHost_pipeline_xml, echo = FALSE, comment = NA
+cat(readLines("examples/simpleInputHost/pipeline.xml"), sep = "\n")
+
+## createVagrant_xml, echo = FALSE, comment = NA
+cat(readLines("examples/simpleInputHost/createVagrant.xml"), sep = "\n")
+
+## normalList_xml, echo = FALSE, comment = NA
+cat(readLines("examples/simpleInputHost/normalList.xml"), sep = "\n")
+
+## run_simpleInputHost
 simpleInputHost <- loadPipeline(
     name = "simpleInputHost",
     ref = file.path("examples", "simpleInputHost", "pipeline.xml"))
 
 result1 <- runPipeline(pipeline = simpleInputHost)
-export(result1)
+result1Tarball <- export(result1)
+
